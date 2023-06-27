d6c74c46cb251cc18d1b0c76c6197d38ababd75c
Abhijit Kar <abhijit.kar@derpycoder.com>
2023-06-27 19:29:40 +0530

    Changed autofill color.

--- lib/derpy_tools_web/components/core_components.ex
+++ lib/derpy_tools_web/components/core_components.ex
@@ -386,7 +386,7 @@ defmodule DerpyToolsWeb.CoreComponents do
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
         class={[
-          "w-full form-input peer rounded-lg border bg-transparent px-3 py-2 placeholder:text-slate-400/70 hover:z-10",
+          "w-full form-input peer rounded-lg border bg-transparent px-3 py-2 placeholder:text-slate-400/70 hover:z-10 autofill:!text-black autofill:!bg-yellow-300",
           @errors == [] &&
             "border-slate-300 hover:border-slate-400 focus:z-10 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent",
           @errors != [] && "border-rose-400 focus:border-rose-400",
