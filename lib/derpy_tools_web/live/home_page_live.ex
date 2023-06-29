fca50eb230c2efac00dcd1fd02e957a30c58d642
Abhijit Kar <abhijit.kar@derpycoder.com>
2023-06-29 19:05:09 +0530

    Redesigned Homepage

--- lib/derpy_tools_web/live/home_page_live.ex
+++ lib/derpy_tools_web/live/home_page_live.ex
@@ -3,25 +3,34 @@ defmodule DerpyToolsWeb.HomePageLive do
 
   def render(assigns) do
     ~H"""
-    <ol class="ml-10 mt-12 list-decimal text-md">
-      <li>
-        <.link
+    <div class="card mt-12">
+      <div class="border-b border-slate-200 p-4 dark:border-navy-500 sm:px-5">
+        <h2 class="font-medium tracking-wide text-slate-700 line-clamp-1 dark:text-navy-100 lg:text-base">
+          <.icon class="hero-link" />
+          URL Beaver
+        </h2>
+      </div>
+      <div class="p-4 sm:px-5">
+        <ul class="space-y-1.5 font-inter font-medium">
+          <li>
+            <.link
           navigate={~p"/utm-builder"}
-          class="text-primary transition-colors hover:text-primary-focus dark:text-accent-light dark:hover:text-accent"
+          class="flex rounded-lg bg-slate-150 bg-gradient-to-r from-purple-500 to-purple-600 px-4 py-2.5 tracking-wide text-white outline-none transition-all"
         >
           UTM Builder
         </.link>
-      </li>
-      <li>
-        <.link
+          </li>
+          <li>
+            <.link
           navigate={~p"/metadata-analyzer"}
-          class="text-primary transition-colors hover:text-primary-focus dark:text-accent-light dark:hover:text-accent"
+          class="flex rounded-lg px-4 py-2.5 tracking-wide outline-none transition-all hover:bg-slate-100 hover:text-slate-800 focus:bg-slate-100 focus:text-slate-800 dark:hover:bg-navy-600 dark:hover:text-navy-100 dark:focus:bg-navy-600 dark:focus:text-navy-100"
         >
-          Meta Data Analyzer (To see if images will load in Twitter, Insta)
+          Meta Data Analyzer
         </.link>
-      </li>
-      <li></li>
-    </ol>
+          </li>
+        </ul>
+      </div>
+    </div>
     """
   end
 
