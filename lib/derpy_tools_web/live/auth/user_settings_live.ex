0f02b020d1514fc2f9f136634f9cd0b337aee583
Abhijit Kar <abhijit.kar@derpycoder.com>
2023-06-29 12:19:29 +0530

    Settings page styled.

--- lib/derpy_tools_web/live/auth/user_settings_live.ex
+++ lib/derpy_tools_web/live/auth/user_settings_live.ex
@@ -5,36 +5,49 @@ defmodule DerpyToolsWeb.UserSettingsLive do
 
   def render(assigns) do
     ~H"""
-    <.header class="text-center">
-      Account Settings
-      <:subtitle>Manage your account email address and password settings</:subtitle>
-    </.header>
-
-    <div class="space-y-12 divide-y">
-      <div>
-        <.simple_form
+    <div class="grid w-full h-screen-sm grow grid-cols-1 place-items-center">
+      <div class="w-full max-w-[26rem] p-4 sm:px-5">
+        <div class="flex flex-col items-center">
+          <.icon class="hero-user-solid w-20 h-20" />
+        </div>
+        <div class="mt-4 text-center">
+          <h2 class="text-2xl font-semibold text-slate-600 dark:text-navy-100">
+            Account Settings
+          </h2>
+          <p class="text-slate-400 dark:text-navy-300">
+            Manage your account email address and password settings
+          </p>
+        </div>
+        <.form
           for={@email_form}
           id="email_form"
           phx-submit="update_email"
           phx-change="validate_email"
+          class="card mt-5 rounded-lg p-5 lg:p-7"
         >
-          <.input field={@email_form[:email]} type="email" label="Email" required />
+          <.input field={@email_form[:email]} type="email" label="Email" required label="Email:">
+            <:icon>
+              <.icon class="hero-at-symbol h-5 w-5 transition-colors duration-200" />
+            </:icon>
+          </.input>
           <.input
             field={@email_form[:current_password]}
             name="current_password"
             id="current_password_for_email"
             type="password"
-            label="Current password"
+            label="Password:"
             value={@email_form_current_password}
             required
-          />
-          <:actions>
-            <.button phx-disable-with="Changing...">Change Email</.button>
-          </:actions>
-        </.simple_form>
-      </div>
-      <div>
-        <.simple_form
+            class="mt-4"
+          >
+            <:icon>
+              <.icon class="hero-lock-closed h-5 w-5 transition-colors duration-200" />
+            </:icon>
+          </.input>
+          <.button phx-disable-with="Changing...">Change Email</.button>
+        </.form>
+
+        <.form
           for={@password_form}
           id="password_form"
           action={~p"/users/log_in?_action=password_updated"}
@@ -42,6 +55,7 @@ defmodule DerpyToolsWeb.UserSettingsLive do
           phx-change="validate_password"
           phx-submit="update_password"
           phx-trigger-action={@trigger_submit}
+          class="card mt-5 rounded-lg p-5 lg:p-7"
         >
           <.input
             field={@password_form[:email]}
@@ -49,25 +63,44 @@ defmodule DerpyToolsWeb.UserSettingsLive do
             id="hidden_user_email"
             value={@current_email}
           />
-          <.input field={@password_form[:password]} type="password" label="New password" required />
+          <.input
+            field={@password_form[:password]}
+            type="password"
+            label="New Password:"
+            required
+            class="mt-4"
+          >
+            <:icon>
+              <.icon class="hero-lock-closed h-5 w-5 transition-colors duration-200" />
+            </:icon>
+          </.input>
           <.input
             field={@password_form[:password_confirmation]}
             type="password"
-            label="Confirm new password"
-          />
+            label="Confirm Password:"
+            class="mt-4"
+          >
+            <:icon>
+              <.icon class="hero-lock-closed h-5 w-5 transition-colors duration-200" />
+            </:icon>
+          </.input>
           <.input
             field={@password_form[:current_password]}
             name="current_password"
             type="password"
-            label="Current password"
+            label="Current Password:"
             id="current_password_for_password"
             value={@current_password}
             required
-          />
-          <:actions>
-            <.button phx-disable-with="Changing...">Change Password</.button>
-          </:actions>
-        </.simple_form>
+            class="mt-4"
+          >
+            <:icon>
+              <.icon class="hero-lock-closed h-5 w-5 transition-colors duration-200" />
+            </:icon>
+          </.input>
+
+          <.button phx-disable-with="Changing...">Change Password</.button>
+        </.form>
       </div>
     </div>
     """
