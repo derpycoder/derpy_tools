827748a3c913f27a8c53b2b9d014e504f8d9e56e
Abhijit Kar <abhijit.kar@derpycoder.com>
2023-07-21 17:14:41 +0530

    Some Commands

--- build.sh
+++ build.sh
@@ -11,3 +11,9 @@ MIX_ENV=prod mix assets.deploy
 
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
+
+# Run migrations
+# _build/prod/rel/derpy_tools/bin/derpy_tools eval "DerpyTools.Release.migrate"
+
+# Run seeds
+# MIX_ENV=prod mix run priv/repo/seeds.exs
