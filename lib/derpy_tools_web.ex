512060c4a278f1846e349f9ce6d58cc5bf3cac13
Abhijit Kar <abhijit.kar@derpycoder.com>
2023-07-22 09:20:14 +0530

    Manifest.json made Public

--- lib/derpy_tools_web.ex
+++ lib/derpy_tools_web.ex
@@ -17,7 +17,7 @@ defmodule DerpyToolsWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt manifest.json)
 
   def router do
     quote do
