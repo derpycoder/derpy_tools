9c5128d723a93326aad20e7cb47e6066cd2b3a1b
Abhijit Kar <abhijit.kar@derpycoder.com>
2023-07-28 13:52:20 +0530

    Cache Manifest Disabled

--- lib/derpy_tools_web.ex
+++ lib/derpy_tools_web.ex
@@ -17,7 +17,7 @@ defmodule DerpyToolsWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt cache_manifest.json)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
 
   def router do
     quote do
