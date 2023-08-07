cea1368f446a59d8c812dbd8793fcd6a19163171
Abhijit Kar <abhijit.kar@derpycoder.com>
2023-08-07 20:51:16 +0530

    Nested Table of Contents done.

--- lib/derpy_tools_web/live/blog/blog_posts.ex
+++ lib/derpy_tools_web/live/blog/blog_posts.ex
@@ -7,7 +7,6 @@ defmodule DerpyToolsWeb.BlogPosts do
   embed_templates "sections/**/*"
 
   attr :headers, :list
-  attr :class, :string, default: nil
 
   def table_of_contents(assigns) do
     ~H"""
@@ -16,42 +15,51 @@ defmodule DerpyToolsWeb.BlogPosts do
       class="sticky top-[calc(var(--header-height))] max-h-[calc(100svh-(var(--header-height)))] overflow-auto px-5 py-1"
       phx-hook="TableOfContents"
     >
-      <h5 class="text-slate-900 font-semibold mb-4 text-sm leading-6 dark:text-slate-100">
+      <h5 class="text-slate-900 font-semibold mb-2 text-sm leading-6 dark:text-slate-100">
         On this page
       </h5>
-      <ul class={[
-        "space-y-1 font-inter font-medium list-none not-prose",
-        @class
-      ]}>
-        <li>
-          <a
-            href="#"
-            class="block py-1 font-medium hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300"
-          >
-            Top<i class="hero-chevron-up w-5.5 h-5.5 text-slate-500 dark:text-navy-100" />
-          </a>
-        </li>
-        <li :for={{header, id, title} <- @headers} class="not-prose">
-          <a
-            id={"#{id}-link"}
-            key={id}
-            href={"##{id}"}
-            tabindex="0"
-            class={[
-              "block py-1 font-medium hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300",
-              case header do
-                "h2" -> ""
-                "h3" -> "pl-4"
-                "h4" -> "pl-8"
-              end
-            ]}
-          >
-            <i :if={header in ~w(h3 h4)} class="hero-chevron-right-mini" />
-            <span><%= title %></span>
-          </a>
-        </li>
-      </ul>
+      <a
+        href="#"
+        class="block py-1 font-medium hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300"
+      >
+        <i class="hero-chevron-up w-5.5 h-5.5 text-slate-500 dark:text-navy-100" /> Top
+      </a>
+
+      <.nested_header headers={@headers} />
     </div>
+    """
+  end
+
+  attr :headers, :list
+  attr :class, :string, default: nil
+
+  def nested_header(assigns) do
+    ~H"""
+    <ul class={["space-y-1 font-inter font-medium list-none not-prose", @class]}>
+      <li
+        :for={%{header: header, id: id, title: title, children: children} <- @headers}
+        class="not-prose"
+      >
+        <a
+          id={"#{id}-link"}
+          key={id}
+          href={"##{id}"}
+          tabindex="0"
+          class={[
+            "block py-1 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300",
+            case header do
+              "h2" -> "font-semibold"
+              "h3" -> "font-medium"
+              "h4" -> "font-normal"
+            end
+          ]}
+        >
+          <i :if={header in ~w{h3 h4}} class="hero-chevron-right-mini" />
+          <span><%= title %></span>
+        </a>
+        <.nested_header :if={children} headers={children |> Enum.reverse()} class="pl-4" />
+      </li>
+    </ul>
     """
   end
 
@@ -154,13 +162,6 @@ defmodule DerpyToolsWeb.BlogPosts do
     |> Enum.filter(fn each -> Tuple.to_list(each) |> Enum.count() == 3 end)
     |> Enum.filter(fn {name, _, _} -> name in ~w(h2 h3 h4) end)
     |> Enum.map(fn
-      {header, meta, [title]} ->
-        {
-          header,
-          meta |> Enum.find(&(elem(&1, 0) == "id")) |> elem(1),
-          title |> String.replace(~r"\n\s+", "")
-        }
-
       {header, meta, [title | _rest]} ->
         {
           header,
@@ -168,6 +169,39 @@ defmodule DerpyToolsWeb.BlogPosts do
           title |> String.replace(~r"\n\s+", "")
         }
     end)
+    |> Enum.reduce([], fn
+      {"h2", id, title}, acc ->
+        [%{header: "h2", id: id, title: title, children: []} | acc]
+
+      {"h3", id, title}, [head | tail] ->
+        [
+          %{
+            head
+            | children: [%{header: "h3", id: id, title: title, children: []} | head.children]
+          }
+          | tail
+        ]
+
+      {"h4", id, title}, [head | tail] ->
+        [child_head | child_tail] = head.children
+
+        [
+          %{
+            head
+            | children: [
+                %{
+                  child_head
+                  | children: [
+                      %{header: "h4", id: id, title: title, children: nil} | child_head.children
+                    ]
+                }
+                | child_tail
+              ]
+          }
+          | tail
+        ]
+    end)
+    |> Enum.reverse()
   end
 
   defp reading_time(parsed_blog) do
@@ -181,28 +215,3 @@ defmodule DerpyToolsWeb.BlogPosts do
     |> Timex.Format.Duration.Formatter.format(:humanized)
   end
 end
-
-# |> Enum.reduce([], fn
-#   {"h2", id, title}, acc ->
-#     [%{id: id, title: title, children: []} | acc]
-
-#   {"h3", id, title}, [head | tail] ->
-#     [%{head | children: [%{id: id, title: title, children: []} | head.children]} | tail]
-
-#   {"h4", id, title}, [head | tail] ->
-#     [child_head | child_tail] = head.children
-
-#     [
-#       %{
-#         head
-#         | children: [
-#             %{
-#               child_head
-#               | children: [%{id: id, title: title, children: nil} | child_head.children]
-#             }
-#             | child_tail
-#           ]
-#       }
-#       | tail
-#     ]
-# end)
