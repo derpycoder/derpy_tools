974e457770586084a12bd7ad77ed80dff2ed4d34
Abhijit Kar <abhijit.kar@derpycoder.com>
2023-08-12 22:37:45 +0530

    Updated ecto extras dependency.

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule DerpyTools.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:ecto_sqlite3, ">= 0.10.3"},
-      {:ecto_sqlite3_extras, "~> 1.1.7"},
+      {:ecto_sqlite3_extras, "~> 1.2.0"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.19.0"},
