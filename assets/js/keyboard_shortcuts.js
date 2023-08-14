76b8923dc2c9a202c6dbfb75bb24d92db1bc9265
Abhijit Kar <abhijit.kar@derpycoder.com>
2023-08-14 12:42:28 +0530

    Corrected issue with Keyboard Shortcut.

--- assets/js/keyboard_shortcuts.js
+++ assets/js/keyboard_shortcuts.js
@@ -1,6 +1,7 @@
 export const CommandPalette = {
   mounted() {
     document.addEventListener("keydown", (e) => {
+      e.preventDefault();
       if (e.key === "k" && e.metaKey) {
         this.el.focus();
       }
@@ -11,6 +12,7 @@ export const CommandPalette = {
 export const PrimaryInput = {
   mounted() {
     document.addEventListener("keyup", (e) => {
+      e.preventDefault();
       if (e.key === "/" && document.activeElement.id !== "search-box") {
         this.el.focus();
       }
