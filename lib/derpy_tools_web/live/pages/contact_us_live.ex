eff233ba5e293913d6e428e1edd1546de508a461
Abhijit Kar <abhijit.kar@derpycoder.com>
2023-08-18 01:02:08 +0530

    Inline Clip Path moved to separate CSS file.

--- lib/derpy_tools_web/live/pages/contact_us_live.ex
+++ lib/derpy_tools_web/live/pages/contact_us_live.ex
@@ -38,10 +38,7 @@ defmodule DerpyToolsWeb.ContactUsLive do
                 class="absolute -left-56 top-[calc(100%-13rem)] transform-gpu blur-3xl lg:left-[max(-14rem,calc(100%-59rem))] lg:top-[calc(50%-7rem)]"
                 aria-hidden="true"
               >
-                <div
-                  class="aspect-[1155/678] w-[72.1875rem] bg-gray-100 opacity-20 dark:from-[#80caff] dark:to-[#4f46e5] dark:bg-gradient-to-br"
-                  style="clip-path: polygon(74.1% 56.1%, 100% 38.6%, 97.5% 73.3%, 85.5% 100%, 80.7% 98.2%, 72.5% 67.7%, 60.2% 37.8%, 52.4% 32.2%, 47.5% 41.9%, 45.2% 65.8%, 27.5% 23.5%, 0.1% 35.4%, 17.9% 0.1%, 27.6% 23.5%, 76.1% 2.6%, 74.1% 56.1%)"
-                >
+                <div class="ethereal-polygon-clip aspect-[1155/678] w-[72.1875rem] bg-gray-100 opacity-20 dark:from-[#80caff] dark:to-[#4f46e5] dark:bg-gradient-to-br">
                 </div>
               </div>
             </div>
